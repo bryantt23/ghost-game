d522f259b89feb7e5d768f2a96423b35d03c52d3
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-03 23:35:21 -0800

    get dictionary info

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 // https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
 function readTextFile(file) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.open('GET', file, false);
-  rawFile.onreadystatechange = function () {
-    if (rawFile.readyState === 4) {
-      if (rawFile.status === 200 || rawFile.status == 0) {
-        var allText = rawFile.responseText;
-        console.log(allText);
+  //https://stackoverflow.com/questions/48969495/in-javascript-how-do-i-should-i-use-async-await-with-xmlhttprequest
+  return new Promise(function (resolve, reject) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open('GET', file, false);
+    rawFile.onreadystatechange = function () {
+      if (rawFile.readyState === 4) {
+        if (rawFile.status === 200 || rawFile.status == 0) {
+          var allText = rawFile.responseText;
+          return resolve(allText.split('\n'));
+        }
       }
-    }
-  };
-  rawFile.send(null);
+    };
+    rawFile.send(null);
+  });
 }
 
-console.log(readTextFile('./dictionary.txt'));
+async function buildDictionary() {
+  const dictionary = await readTextFile('./dictionary.txt');
+  console.log(dictionary);
+}
+
+buildDictionary();
+// console.log(readTextFile('./dictionary.txt'));
