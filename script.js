e0897fd158cd1887cdfb5c448d861763ed35d0d3
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-04 00:24:46 -0800

    Add methods for phase 1

--- script.js
+++ script.js
@@ -28,22 +28,77 @@ class Player {
     this.name = name;
   }
 
-  guess() {}
+  guess = () => {
+    const letter = prompt(`${this.name} Enter a letter`);
+    if (!letter) {
+      return;
+    }
+    return letter;
+  };
 
-  alertInvalidGuess() {}
+  alertInvalidGuess = () => {
+    console.log('You need to select another letter');
+  };
 }
 
 class Game {
   constructor(dictionary, player1, player2) {
     this.fragment = '';
-    this.dictionary = dictionary;
+    this.dictionary = new Set(dictionary);
+    this.currentPlayer = player1;
+    this.previousPlayer = player2;
+  }
+
+  start() {
+    console.log(this.isGameOver());
+    while (!this.isGameOver()) {
+      this.takeTurn(this.currentPlayer);
+      console.log(`The current fragment is: ${this.fragment}`);
+    }
+  }
+
+  nextPlayer() {
+    let temp = this.currentPlayer;
+    this.currentPlayer = this.previousPlayer;
+    this.previousPlayer = temp;
+  }
+
+  takeTurn(player) {
+    console.log(`It is ${player.name}'s turn`);
+    const playerLetter = player.guess();
+    let tempFragment = this.fragment + playerLetter;
+    if (this.isValidPlay(tempFragment)) {
+      this.fragment = tempFragment;
+      console.log(`${player.name} has made the fragment into ${this.fragment}`);
+      this.nextPlayer();
+    } else {
+      player.alertInvalidGuess();
+    }
+  }
+
+  isValidPlay(fragment) {
+    return (
+      Array.from(this.dictionary).findIndex(word => word.startsWith(fragment)) >
+      -1
+    );
+  }
+
+  isGameOver() {
+    if (this.dictionary.has(this.fragment)) {
+      console.log(`Game over. The winner is ${this.currentPlayer.name}`);
+      return true;
+    }
+    return;
   }
 }
 
 async function start() {
   const dict = await buildDictionary();
-  const game = new Game(dict);
+  const player1 = new Player('Abe'),
+    player2 = new Player('Bri');
+  const game = new Game(dict, player1, player2);
   console.log(game);
+  game.start();
 }
 
 start();
